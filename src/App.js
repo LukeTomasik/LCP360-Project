6ec139a6b6b6f9649d6f10b252cf07359e2e3296
NoxYeah <luke.tomasik@gmail.com>
2021-03-26 18:04:32 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Map2 from './components/Map2'
+import Logo from './components/Logo'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+  
+
+    <Map2/>
+      
+    
+    </div>
+  );
+}
+
+export default App;
